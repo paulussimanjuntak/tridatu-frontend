3a081b7ab5aeb4bf6147019e2e178d6e063905aa
paulussimanjuntak <paulusbsimanjuntak@gmail.com>
2020-10-14 17:04:15 +0800

    update store information home 🏠

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ const CardProductMemo = React.memo(CardProduct);
 const CardBrandMemo = React.memo(CardBrand);
 const CardBannerMemo = React.memo(CardBanner);
 
-import { brandSettings, bannerSettings, infoStoreSettings } from "lib/slickSetting";
+import { brandSettings, bannerSettings, infoStoreSettings, infoStoreSettingsMobile } from "lib/slickSetting";
 
 // let banners = ['https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/5/53863f31-bd90-437a-8a6d-da203de8682f.jpg', 'https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/8/cafdfacb-e36c-49af-b917-d62b3e5e2131.jpg', 'https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/8/c66dd503-4300-40a6-bb32-a785e14dd563.jpg','https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/5/53863f31-bd90-437a-8a6d-da203de8682f.jpg', 'https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/8/cafdfacb-e36c-49af-b917-d62b3e5e2131.jpg', 'https://ecs7-p.tokopedia.net/img/cache/750/NsjrJu/2020/9/8/c66dd503-4300-40a6-bb32-a785e14dd563.jpg']
 let banners = ['https://s4.bukalapak.com/rev-banner/flash_banner/852356698/s-628-412/desktop_BukamartIDC_a5b7b80c-4a86-4e19-9e4c-d9082e266604.jpeg','https://s4.bukalapak.com/rev-banner/flash_banner/852356698/s-628-412/desktop_BukamartIDC_a5b7b80c-4a86-4e19-9e4c-d9082e266604.jpeg','https://s4.bukalapak.com/rev-banner/flash_banner/852356698/s-628-412/desktop_BukamartIDC_a5b7b80c-4a86-4e19-9e4c-d9082e266604.jpeg','https://s4.bukalapak.com/rev-banner/flash_banner/852356698/s-628-412/desktop_BukamartIDC_a5b7b80c-4a86-4e19-9e4c-d9082e266604.jpeg','https://s4.bukalapak.com/rev-banner/flash_banner/852356698/s-628-412/desktop_BukamartIDC_a5b7b80c-4a86-4e19-9e4c-d9082e266604.jpeg',]
@@ -34,43 +34,52 @@ const Home = () => {
           </Form>
         </section>
 
-        {/* <Row> */}
-        {/*   <Col lg={10} md={12}> */}
-        {/*     <section className="banner-section"> */}
-        {/*       <h4 className="fs-20-s">Promo</h4> */}
-        {/*       <Slider {...bannerSettings}> */}
-        {/*         {banners.map((data, i) => ( */}
-        {/*           <Col key={i} className="px-0"> */}
-        {/*             <CardBannerMemo image={data} /> */}
-        {/*           </Col> */}
-        {/*         ))} */}
-        {/*       </Slider> */}
-        {/*     </section> */}
+        <Row>
+          <Col lg={10} md={12}>
+            <section className="banner-section">
+              <h4 className="fs-20-s">Promo</h4>
+              <Slider {...bannerSettings}>
+                {banners.map((data, i) => (
+                  <Col key={i} className="px-0">
+                    <CardBannerMemo image={data} />
+                  </Col>
+                ))}
+              </Slider>
+            </section>
 
-        {/*     <section className="brand-section"> */}
-        {/*       <h4 className="fs-20-s mb-4">Brand</h4> */}
-        {/*       <Slider {...brandSettings}> */}
-        {/*         {[...Array(6)].map((_, i) => ( */}
-        {/*           <Col key={i} className="px-0"> */}
-        {/*             <CardBrandMemo /> */}
-        {/*           </Col> */}
-        {/*         ))} */}
-        {/*       </Slider> */}
-        {/*     </section> */}
-        {/*   </Col> */}
+            <section className="brand-section">
+              <h4 className="fs-20-s mb-4">Brand</h4>
+              <Slider {...brandSettings}>
+                {[...Array(6)].map((_, i) => (
+                  <Col key={i} className="px-0">
+                    <CardBrandMemo />
+                  </Col>
+                ))}
+              </Slider>
+            </section>
+          </Col>
 
-        {/*   <Col lg={2} md={12}> */}
-        {/*     <section className="info-store"> */}
-        {/*       <h4 className="fs-20-s info-store-title mb-3">Informasi Outlet</h4> */}
-        {/*       <Slider {...infoStoreSettings}> */}
-        {/*         {infoStores.map((data, i) => ( */}
-        {/*           <img src={data} className="mb-2 info-store-img" alt="Tridatu Bali ID" key={i} /> */}
-        {/*         ))} */}
-        {/*       </Slider> */}
-        {/*     </section> */}
-        {/*   </Col> */}
-        {/* </Row> */}
+          <Col lg={2} md={12} sm={12} className="d-none d-lg-block">
+            <section className="info-store">
+              <h4 className="fs-20-s info-store-title mb-3">Informasi Outlet</h4>
+              <Slider {...infoStoreSettings}>
+                {infoStores.map((data, i) => (
+                  <img src={data} className="mb-2 info-store-img" alt="Tridatu Bali ID" key={i} />
+                ))}
+              </Slider>
+            </section>
+          </Col>
+        </Row>
 
+        <section className="info-store d-block d-lg-none">
+          <h4 className="fs-20-s info-store-title mb-3">Informasi Outlet</h4>
+          <Slider {...infoStoreSettingsMobile}>
+            {infoStores.map((data, i) => (
+              <img src={data} className="mb-2 info-store-img" alt="Tridatu Bali ID" key={i} />
+            ))}
+          </Slider>
+        </section>
+        
         <section>
           <h4 className="fs-20-s mb-4">Paling Banyak Dilihat</h4>
           <Row className="row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 custom-gutters">
