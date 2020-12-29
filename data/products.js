cdd69745b71a1cae56255706fd88936af36d8629
paulussimanjuntak <paulusbsimanjuntak@gmail.com>
2020-12-30 02:21:36 +0800

    🎗  seperate variant data

--- data/products.js
+++ data/products.js
@@ -1,1 +1,45 @@
-export const productsData = [ 'Jaket GAP Grey', 'Flannel Uniqlo Biru', 'Kemeja Converse Panjang' ]
+export const productsData = [
+  "Jaket GAP Grey",
+  "Flannel Uniqlo Biru",
+  "Kemeja Converse Panjang",
+];
+
+export const formItemLayout = {
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 24 },
+    md: { span: 18 },
+    lg: { span: 14 },
+    xl: { span: 12 },
+  },
+};
+
+export const initialColumn = [
+  {
+    title: "Harga",
+    dataIndex: "price",
+    key: "price",
+    inputType: "price",
+    editable: true,
+    align: "center",
+    width: 150,
+  },
+  {
+    title: "Stok",
+    dataIndex: "stock",
+    key: "stock",
+    inputType: "stock",
+    editable: true,
+    align: "center",
+    width: 150,
+  },
+  {
+    title: "Kode Variasi",
+    dataIndex: "code",
+    key: "code",
+    inputType: "code",
+    editable: true,
+    align: "center",
+    width: 150,
+  },
+];
