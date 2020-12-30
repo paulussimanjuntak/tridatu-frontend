882734da9800b78735fcc970b74c1fe0594ae8a5
paulussimanjuntak <paulusbsimanjuntak@gmail.com>
2020-12-30 17:02:51 +0800

    add size guide example in new product

---
+++ components/Modal/Admin/Products/SizeGuide.jsx
@@ -0,0 +1,31 @@
+import { Modal, Image } from 'antd'
+
+const SizeGuide = ({ show, close, image }) => {
+  return(
+    <>
+      <Modal
+        centered
+        title=" "
+        visible={show}
+        onCancel={close}
+        zIndex="3030"
+        closeIcon={<i className="fas fa-times" />}
+        footer={null}
+        className="modal-sizeguide"
+        width={650}
+        bodyStyle={{padding: '0'}}
+      >
+        <Image src={image} />
+      </Modal>
+
+      <style jsx>{`
+      :global(.modal-sizeguide > .ant-modal-content, 
+        .modal-sizeguide > .ant-modal-content > .ant-modal-header) {
+        border: unset;
+      }
+      `}</style>
+    </>
+  )
+}
+
+export default SizeGuide
